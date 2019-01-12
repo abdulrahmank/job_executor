8583e7f0b625a6aa51b09fc45762c464fc874eac
abdulrahmank <kadkab.abdul@gmail.com>
2019-01-12 17:22:13 +0530

    Save file and setting given as a part of job creation request

--- time_based/persistor.go
+++ time_based/persistor.go
@@ -1,5 +1,13 @@
 package time_based
 
-func SaveJob(jobName, timeSlots, daysInWeek, fileName string, numberOfWeeks int, jobFileContent []byte) {
+import "github.com/abdulrahmank/job_executor/time_based/dao"
 
+type Persistor struct {
+	FileDao    dao.FileDao
+	SettingDao dao.JobSettingDao
+}
+
+func (p *Persistor) SaveJob(jobName, timeSlots, daysInWeek, fileName string, numberOfWeeks int, jobFileContent []byte) {
+	p.FileDao.SaveFile(fileName, jobFileContent)
+	p.SettingDao.SaveJob(jobName, timeSlots, daysInWeek, fileName, numberOfWeeks)
 }
--- time_based/persistor_test.go
+++ time_based/persistor_test.go
@@ -1,11 +1,30 @@
 package time_based
 
 import (
+	"github.com/abdulrahmank/job_executor/time_based/mocks"
+	"github.com/golang/mock/gomock"
 	"testing"
 )
 
 func TestSaveJob(t *testing.T) {
 	t.Run("Should save given job", func(t *testing.T) {
-		
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		mockFileDao := mocks.NewMockFileDao(mockCtrl)
+		mockSettingsDao := mocks.NewMockJobSettingDao(mockCtrl)
+
+		persistor := &Persistor{FileDao: mockFileDao, SettingDao: mockSettingsDao}
+		jobName := "hw"
+		timeSlots := "10:00,11:00"
+		daysInWeek := "wed,fri"
+		fileName := "hw.sh"
+		content := []byte("echo 'hello world'")
+		numberOfWeeks := 4
+
+		mockFileDao.EXPECT().SaveFile(fileName, content)
+		mockSettingsDao.EXPECT().SaveJob(jobName, timeSlots, daysInWeek, fileName, numberOfWeeks)
+
+		persistor.SaveJob(jobName, timeSlots, daysInWeek, fileName, numberOfWeeks, content)
 	})
 }
