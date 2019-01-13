8414473dd9ee5cdddc6d7d4b70e6c4f892523ce3
abdulrahmank <kadkab.abdul@gmail.com>
2019-01-13 11:52:50 +0530

    Make orchestrator sync jobs when a job is created

--- handler/main.go
+++ handler/main.go
@@ -2,7 +2,10 @@ package handler
 
 import (
 	"github.com/abdulrahmank/job_executor/time_based/dao"
+	"github.com/abdulrahmank/job_executor/time_based/exector"
+	"github.com/abdulrahmank/job_executor/time_based/orchestrator"
 	"github.com/abdulrahmank/job_executor/time_based/persistor"
+	"github.com/abdulrahmank/job_executor/time_based/scheduler"
 	"log"
 	"net/http"
 	"strconv"
@@ -29,8 +32,12 @@ func TimedJobScheduler(w http.ResponseWriter, r *http.Request) {
 		if _, err = file.Read(contents); err != nil {
 			log.Println("Error reading file contents")
 		}
-		persistor := persistor.Persistor{FileDao: &dao.FileDaoImpl{}, SettingDao: &dao.JobSettingDaoImpl{}}
-		persistor.SaveJob(jobName, timeSlots, daysInWeek, fileHeader.Filename, numberOfWeeks, contents)
+		p := getPersistor()
+		jobOrchestrator := getOrchestrator()
+
+		p.SaveJob(jobName, timeSlots, daysInWeek, fileHeader.Filename, numberOfWeeks, contents)
+		jobOrchestrator.SyncJobs()
+
 		w.WriteHeader(http.StatusOK)
 		if _, err := w.Write([]byte("Job saved successfully")); err != nil {
 			log.Fatal("Unable to wrtie to response")
@@ -44,4 +51,14 @@ func TimedJobScheduler(w http.ResponseWriter, r *http.Request) {
 		break
 	}
 
+}
+
+func getOrchestrator() orchestrator.JobOrchestrator {
+	return orchestrator.JobOrchestrator{Scheduler:
+	&scheduler.TimeBasedSchedulerImpl{Executor: &exector.BashExecutorImp{}},
+		SettingsDao: &dao.JobSettingDaoImpl{}}
+}
+
+func getPersistor() persistor.Persistor {
+	return persistor.Persistor{FileDao: &dao.FileDaoImpl{}, SettingDao: &dao.JobSettingDaoImpl{}}
 }
